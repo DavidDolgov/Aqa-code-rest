d578a68bad55fdccd6d2ab1018e9a77bd76e6f02
Dolgov David <dolgov.david.1993@gmail.com>
2023-06-02 23:16:28 +0300

    Inital commit

---
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -0,0 +1,63 @@
+package ru.netology.rest;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+class MobileBankApiTestV1 {
+    @Test
+    void shouldReturnDemoAccounts() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    void shouldReturnInfoForDemoAccounts1() {
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                .when()
+                .get("/demo/accounts")
+                .then()
+                .contentType(ContentType.JSON);
+    }
+
+    @Test
+    void shouldReturnInfoForDemoAccounts2() {
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                .when()
+                .get("/demo/accounts")
+                .then()
+                .body("", hasSize(3));
+    }
+
+    @Test
+    void shouldReturnInfoForDemoAccounts3() {
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                .when()
+                .get("/demo/accounts")
+                .then()
+                .body("[1].currency",equalTo("USD"));
+    }
+
+    @Test
+    void shouldReturnInfoForDemoAccounts4() {
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                .when()
+                .get("/demo/accounts")
+                .then()
+                .body("every {it.balance >= 0}",is(true));
+    }
+}
