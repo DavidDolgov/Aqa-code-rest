071663455d49da1bfe85aa6906a4b2e36e8458b1
Dolgov David <dolgov.david.1993@gmail.com>
2023-06-06 20:09:31 +0300

    JSON Schema

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -4,6 +4,7 @@ import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.hamcrest.Matchers.*;
 
 class MobileBankApiTestV1 {
@@ -59,5 +60,15 @@ class MobileBankApiTestV1 {
                 .get("/demo/accounts")
                 .then()
                 .body("every {it.balance >= 0}",is(true));
+    }
+    @Test
+    void shouldReturnInfoForDemoAccounts5() {
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                .when()
+                .get("/demo/accounts")
+                .then()
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+        ;
     }
 }
